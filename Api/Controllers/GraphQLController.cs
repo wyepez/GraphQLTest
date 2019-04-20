b99ea611a75cd310d3415c06f3ee194b10e132dc
Washington Xavier Yépez Yépez <wyepez@outlook.com>
2019-04-20 16:22:06 -0500

    Add project files.

---
+++ Api/Controllers/GraphQLController.cs
@@ -0,0 +1,43 @@
+﻿using Api.Database;
+using Api.GraphQL;
+using GraphQL;
+using GraphQL.Types;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("graphql")]
+    [ApiController]
+    public class GraphQLController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public GraphQLController(ApplicationDbContext db) => this.db = db;
+
+        public async Task<IActionResult> Post([FromBody] GraphQLQuery query)
+        {
+            var inputs = query.Variables.ToInputs();
+
+            var schema = new Schema
+            {
+                Query = new AuthorQuery(db)
+            };
+
+            var result = await new DocumentExecuter().ExecuteAsync(executionOptions =>
+            {
+                executionOptions.Schema = schema;
+                executionOptions.Query = query.Query;
+                executionOptions.OperationName = query.OperationName;
+                executionOptions.Inputs = inputs;
+            });
+
+            if (result.Errors?.Count > 0)
+            {
+                return BadRequest();
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Api/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Api.Database;
+using GraphiQl;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<ApplicationDbContext>(context =>
+            {
+                context.UseInMemoryDatabase("OktaGraphQL");
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseGraphiQl("/graphql");
+            app.UseMvc();
+        }
+    }
+}
